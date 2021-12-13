d10386d6dbb22303f3ba39dfa4cdaebca37b6f94
yamil-90 <yamil.coloma90@gmail.com>
2021-12-13 01:40:49 -0300

    added continuous sound if pressed down

--- components/screens/FartScreen.js
+++ components/screens/FartScreen.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Text, View, Animated } from 'react-native';
 import { StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import { Audio } from "expo-av";
@@ -8,59 +8,76 @@ import { Audio } from "expo-av";
 
 
 export default function FartScreen() {
-  const [position] = useState(new Animated.Value(-15))
-  
+  const [position] = useState(new Animated.Value(-15));
+  const pressed= useRef(false)
+
   const playSoundAsync = async () => {
     try {
       const sound = new Audio.Sound();
-    await sound.loadAsync(
-      require('../../assets/sounds/fart01.mp3')
-    );
-    await sound.playAsync();
+      await sound.loadAsync(
+        require('../../assets/sounds/fart01.mp3')
+      );
+      await sound.playAsync();
     } catch (err) {
       console.log(err)
     }
   }
-  const handleButtonDown=()=>{
+  const handleButtonDown = () => {
+    pressed.current = true
     console.log('fart in');
     playSoundAsync();
-    Animated.timing(position,{
+    Animated.timing(position, {
       toValue: 0,
-      duration:50,
+      duration: 50,
       useNativeDriver: false,
-    }).start()
+    }).start();
   }
 
-  const handleButtonUp=()=>{
+  const handleButtonUp = () => {
+    pressed.current = false;
     console.log('fart out');
     Animated.timing(position, {
       toValue: -13,
       duration: 50,
       useNativeDriver: false,
     }).start();
+  
+  }
+
+  const handleLongPress = () => {
+    
+    const loop = ()=>{
+      // console.log(pressed.current)
+      if(pressed.current){
+      playSoundAsync()
+      } else (
+        clearInterval(timer)
+      )
+    }
+     const timer = setInterval(loop, 500)
+    
+    
   }
   return (
     <View style={styles.container}>
-      {/* <Text style={styles.title}>
-        fart
-      </Text> */}
+      <Text>{pressed ? 'true' : 'false'}</Text>
       <TouchableWithoutFeedback
         touchSoundDisabled={true}
         onPressIn={handleButtonDown}
         onPressOut={handleButtonUp}
-        onLongPress={()=>console.log('long')}
+        onLongPress={handleLongPress}
       >
-      <View style={styles.button}>
-        <View style={styles.thickness}>
-          <Animated.View 
-          style={[styles.topButton, {top:position}]}>
-            <Text style={styles.white}>Fart</Text>
-          </Animated.View>
+        <View style={styles.button}>
+          <View style={styles.thickness}>
+            <Animated.View
+              style={[styles.topButton, { top: position }]}>
+              <Text style={styles.white}>Fart</Text>
+            </Animated.View>
 
+          </View>
         </View>
-      </View>
       </TouchableWithoutFeedback>
-      
+
     </View>
   )
 }
@@ -78,20 +95,20 @@ const styles = StyleSheet.create({
   button: {
     width: 180,
     height: 100,
-    padding:10, 
-    backgroundColor:'grey',
+    padding: 10,
+    backgroundColor: 'grey',
     borderRadius: 16
   },
-  thickness:{
-    width:'100%', 
-    height:'100%', 
-    backgroundColor:'darkred',
+  thickness: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'darkred',
     borderRadius: 16
   },
-  topButton:{
-    width:'100%', 
-    height:'100%', 
-    backgroundColor:'red',
+  topButton: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'red',
     borderRadius: 16,
     alignItems: "center",
     justifyContent: "center",
